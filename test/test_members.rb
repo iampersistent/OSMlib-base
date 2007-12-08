95ecf96e8daf42039e0d3b8b3172418a8c8acf52
joto <joto@717e3884-936c-430f-b93f-cbfeea02e76b>
2007-12-08 16:16:46 +0000

    Added tests for OSM::Member

---
+++ test/test_members.rb
@@ -0,0 +1,34 @@
+$: << 'lib'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'OSM', 'objects.rb')
+require 'test/unit'
+
+class MembersTest < Test::Unit::TestCase
+
+    def test_node
+        member = OSM::Member.new('node', 17, 'foo')
+        assert_equal 'node', member.type
+        assert_equal 17, member.ref
+        assert_equal 'foo', member.role
+    end
+
+    def test_way
+        member = OSM::Member.new('way', 17)
+        assert_equal 'way', member.type
+        assert_equal 17, member.ref
+        assert_equal '', member.role
+    end
+
+    def test_relation
+        member = OSM::Member.new('relation', 17, 'foo')
+        assert_equal 'relation', member.type
+        assert_equal 17, member.ref
+        assert_equal 'foo', member.role
+    end
+
+    def test_fail
+        assert_raise ArgumentError do
+            OSM::Member.new('unknown', 17, 'foo')
+        end
+    end
+
+end
