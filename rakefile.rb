19ba9be27877f5755cc45e10fcd6deb775d9f6a4
joto <joto@717e3884-936c-430f-b93f-cbfeea02e76b>
2008-02-18 07:51:02 +0000

    some cleanups

--- rakefile.rb
+++ rakefile.rb
@@ -4,11 +4,15 @@ require 'rake/rdoctask'
 require 'rake/gempackagetask'
 #require 'rake/clean'
 
+$: << 'lib'
+
+require 'OSM'
+
 task :default => :test
 
 desc "Run the tests"
 Rake::TestTask::new do |t|
-    t.test_files = FileList['test/test*.rb']
+    t.test_files = FileList['test/test_*.rb']
     t.verbose = true
 end
 
@@ -23,17 +27,17 @@ Rake::RDocTask::new do |rdoc|
     rdoc.rdoc_dir = 'rdoc/'
     rdoc.title    = "OSM Library Documentation - Base"
     rdoc.options << '--line-numbers' << '--inline-source'
-    rdoc.rdoc_files = FileList['README', 'examples/README', 'lib/**/*.rb']
+    rdoc.rdoc_files = FileList['README.txt', 'examples/README.txt', 'lib/**/*.rb']
 end
 
 spec = Gem::Specification::new do |s|
     s.platform = Gem::Platform::RUBY
 
     s.name = 'osmlib-base'
-    s.version = '0.1.1'
+    s.version = OSM::VERSION
     s.summary = 'Library for basic OpenStreetMap data handling'
     s.description = <<EOF
-Basic support for OpenStreetMap data model (Nodes, Ways, Relations and Tags). Parsing of OSM XML files.
+Basic support for OpenStreetMap data model (Nodes, Ways, Relations and Tags). Parsing of OSM XML files. Access to OpenStreetMap API.
 EOF
     s.author = 'Jochen Topf'
     s.email = 'jochen@topf.org'
@@ -44,10 +48,10 @@ EOF
     s.test_files = FileList['test/test*.rb']
 
     s.has_rdoc = true
-    s.extra_rdoc_files = ["README"]
+    s.extra_rdoc_files = ['README.txt', 'examples/README.txt']
 
     s.files = FileList["lib/**/*.rb", "LICENSE", "rakefile.rb"] + s.test_files + s.extra_rdoc_files
-    s.rdoc_options.concat ['--main',  'README']
+    s.rdoc_options.concat ['--main',  'README.txt']
 
     # commented out because this doesn't work when libxml-ruby is installed as a Debian package
     #s.add_dependency('libxml-ruby')
